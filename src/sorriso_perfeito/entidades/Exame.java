1af0d1cb2f1a101d02423ee256a8b774e746f5f8
Leo Abrantes <leoabrantesmelo@gmail.com>
2025-04-21 22:28:43 -0300

    implementando a classe Exam

--- src/sorriso_perfeito/entidades/Exame.java
+++ src/sorriso_perfeito/entidades/Exame.java
@@ -4,5 +4,31 @@ public class Exame extends Servico {
 	
 	private String medida_preventiva;
 	private int tipo_de_exame;
+	
+	public Exame(String medida_preventiva, int tipo_de_exame, String descricao, double preco) {
+		super(descricao, preco);
+		this.medida_preventiva = medida_preventiva;
+		this.tipo_de_exame = tipo_de_exame;
+	}
+
+	public String getMedida_preventiva() {
+		return medida_preventiva;
+	}
+
+	public void setMedida_preventiva(String medida_preventiva) {
+		this.medida_preventiva = medida_preventiva;
+	}
+
+	public int getTipo_de_exame() {
+		return tipo_de_exame;
+	}
+
+	public void setTipo_de_exame(int tipo_de_exame) {
+		this.tipo_de_exame = tipo_de_exame;
+	}
+	
+	
+	
+	
 
 }
--- src/sorriso_perfeito/entidades/Limpeza_Restauracao.java
+++ src/sorriso_perfeito/entidades/Limpeza_Restauracao.java
@@ -1,8 +1,34 @@
 package sorriso_perfeito.entidades;
 
-public class Limpeza_Restauracao {
+public class Limpeza_Restauracao extends Servico {
 		
-		private String material_utilizado;
+		private String material_usado;
 		private String dente_tratado;
+		
+		public Limpeza_Restauracao(String material_usado, String dente_tratado, String descricao, double preco) {
+			super(descricao, preco);
+			this.material_usado = material_usado;
+			this.dente_tratado = dente_tratado;
+		}
+
+		public String getMaterial_usado() {
+			return material_usado;
+		}
+
+		public void setMaterial_usado(String material_usado) {
+			this.material_usado = material_usado;
+		}
+
+		public String getDente_tratado() {
+			return dente_tratado;
+		}
+
+		public void setDente_tratado(String dente_tratado) {
+			this.dente_tratado = dente_tratado;
+		}
+		
+		
+		
+		
 
 }
